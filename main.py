f47178c8455045a435dc594a105ca7e0db099120
Initiatechy <64336110+Initiatechy@users.noreply.github.com>
2021-12-24 00:50:13 +0900

    UPDATE

--- main.py
+++ main.py
@@ -4,6 +4,10 @@
 
 import argparse
 import magnets
+import sys
+import math
+import os
+
 
 
 parser = argparse.ArgumentParser()
@@ -13,18 +17,12 @@ parser.add_argument('--r', default = 2500) # 磁石半径[μm]
 parser.add_argument('--thick', default = 1000) # 磁石厚さ[μm]
 parser.add_argument('--gap', default = 200) # 磁石間ギャップ[μm]
 parser.add_argument('--y', default = 9500) # 中央磁石の移動[μm]
-parser.add_argument('--init_y', default = 9500) # シミュレーションを開始する際の初期位置 いらないかもしれない
 
 parser.add_argument('--m', default = 26) # 磁石m角形近似
 parser.add_argument('--n', default = 2) # 動かす磁石以外の磁石個数
 
-# 測定面用
-parser.add_argument('--mw', default = 10000) # 測定面用幅
-parser.add_argument('--ml', default = 10000) # 測定面用高さ
-
 # パス関連
-# parser.add_argument('path', default="output.txt")  # パスに入力したい名前
-
+parser.add_argument('path') # パスに入力したい名前
 parser.add_argument('--filename', default = 'none')  # ファイル名
 
 # -------------------------------------------------------------------------
@@ -36,30 +34,38 @@ r = float(args.r)
 thick = float(args.thick)
 gap = float(args.gap)
 y = float(args.y)
-init_y = float(args.init_y)
 m = float(args.m)
 n = int(args.n)
-mw = float(args.mw)
-ml = float(args.ml)
 filename = args.filename
 
 print('引数 r : ', args.r)  # 磁石半径[μm]
 print('引数 thick: ', args.thick)  # 磁石厚さ[μm]
 print('引数 gap : ', args.gap)  # 磁石間ギャップ[μm]
 print('引数 y : ', args.y)  # 中央磁石の移動[μm]
-print('引数 init_y : ', args.init_y)  # シミュレーションを開始する際の初期位置 いらないかもしれない
 print('引数 m : ', args.m)  # 磁石m角形近似
 print('引数 n : ', args.n)  # 動かす磁石以外の磁石個数
-print('引数 mw : ', args.mw)  # 測定面用幅
-print('引数 ml : ', args.ml)  # 測定面用高さ
 
 
 # パス調整 - TODO: よしなにする
-# date = datetime.datetime.now()
-# path = os.getcwd()
-# path = path + "\\" + str(filename)
-# path = path + '.Pin'
-path = "output.txt"
+path = os.getcwd()
+path = path + "\\" + str(filename)
+path = path + '.Pin'
+
+
+# 磁石配置用の計算をゴネゴネする - TODO: よしなにする
+d = 2*((4*r + 2*gap)*math.cos(math.radians(45))) # 円形磁石間距離
+
+z_ofst_1 = -thick/2 # 1つめ磁石Z軸オフセット
+z_ofst_2 = -thick/2 # 2つめ磁石Z軸オフセット
+z_ofst_3 = -thick/2 # 3つめ磁石Z軸オフセット
+
+y_ofst_1 = 1000 + r # 1つめ磁石Y軸方向オフセット
+y_ofst_2 = y + y_ofst_1 # 2つめ磁石Y軸方向オフセット
+y_ofst_3 = y_ofst_1 + d/2 # 3つめ磁石Y軸方向オフセット
+
+x_ofst_1 = 1000 + r # 1つめ磁石X軸オフセット
+x_ofst_3 = x_ofst_1 + d/2 # 3つめ磁石X軸オフセット
+x_ofst_2 = (x_ofst_1 + x_ofst_3)/2# 2つめ磁石X軸オフセット
 
 
 # ***** プログラムここから *****
@@ -79,21 +85,24 @@ $nodenum grp on
 
 # 磁石を置く
 ## 1つ目の円形磁石オブジェクトを生成する
-Magnet1 = magnets.CircleMagnet(r=r, thick=thick, m=m, x_ofst=100, y_ofst=200, z_ofst=300, n=n, gap=gap, script=script, group_id=group_id, node_id=node_id)
+Magnet1 = magnets.CircleMagnet(r=r, thick=thick, m=m, x_ofst=x_ofst_1, y_ofst=y_ofst_1, z_ofst=z_ofst_1, n=n, d=d, gap=gap, script=script, group_id=group_id, node_id=node_id)
 ## 円形磁石を配置する
 Magnet1.plant()
-## 円形磁石を測定対象に指定する
-Magnet1.measure()
 ## script, group_id, node_id を返してもらう
 script, group_id, node_id = Magnet1.get_scripts()
 
 
 ## 2つ目の円形磁石オブジェクトを生成する
-Magnet2 = magnets.CircleMagnet(r=r, thick=thick, m=m, x_ofst=200, y_ofst=300, z_ofst=400, n=n, gap=gap, script=script, group_id=group_id, node_id=node_id)
-## 円形磁石を配置する
+Magnet2 = magnets.CircleMagnet(r=r, thick=thick, m=m, x_ofst=x_ofst_2, y_ofst=y_ofst_2, z_ofst=z_ofst_2, n=n-1, d=d, gap=gap, script=script, group_id=group_id, node_id=node_id)
 Magnet2.plant()
-## script, group_id, node_id を返してもらう
+## 円形磁石を測定対象に指定する
+Magnet2.measure_frustum()
 script, group_id, node_id = Magnet2.get_scripts()
+
+## 3つ目の円形磁石オブジェクトを生成する
+Magnet3 = magnets.CircleMagnet(r=r, thick=thick, m=m, x_ofst=x_ofst_3, y_ofst=y_ofst_3, z_ofst=z_ofst_3, n=n, d=d, gap=gap, script=script, group_id=group_id, node_id=node_id)
+Magnet3.plant()
+script, group_id, node_id = Magnet3.get_scripts()
 
 
 # ファイルに出力する
